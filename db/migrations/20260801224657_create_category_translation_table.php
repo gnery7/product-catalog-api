3a43c5d8b8298f15e0662b31d6354cc6e98d9d06
gnery7 <geovanne40@gmail.com>
2026-08-01 20:16:05 -0300

    feat: adicionada a migration da tabela de traducoes de categorias

---
+++ db/migrations/20260801224657_create_category_translation_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateCategoryTranslationTable extends AbstractMigration
+{
+  public function up()
+  {
+    $table = $this->table('category_translation');
+    $table
+      ->addColumn('category_id', 'integer')
+      ->addColumn('lang_code', 'text')
+      ->addColumn('label', 'text')
+      ->addIndex(['category_id', 'lang_code'], ['unique' => true])
+      ->create();
+  }
+
+  public function down()
+  {
+    $this->table('category_translation')->drop()->save();
+  }
+}
