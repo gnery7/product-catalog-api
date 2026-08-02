d09a6b2628b455809633b9a582bf0d250f554ad1
gnery7 <geovanne40@gmail.com>
2026-08-02 18:33:14 -0300

    fix: corrigido tipo de retorno do middleware para seguir a interface PSR-15

--- src/Middleware/JsonResponseMiddleware.php
+++ src/Middleware/JsonResponseMiddleware.php
@@ -2,13 +2,13 @@
 
 namespace Contatoseguro\TesteBackend\Middleware;
 
-use Slim\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class JsonResponseMiddleware
 {
-    public function __invoke(Request $request, RequestHandler $handler): Response
+    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface
     {
         $response = $handler->handle($request);
         return $response->withAddedHeader("Content-Type", "application/json");
