dcb5d9b3276d96abaf778b0fd6833ea026d28b6b
gnery7 <geovanne40@gmail.com>
2026-08-02 22:35:56 -0300

    style: quebrada a linha longa da conexao mysql no DB.php

--- src/Config/DB.php
+++ src/Config/DB.php
@@ -9,7 +9,8 @@ class DB
     public static function connect()
     {
         if (!isset(self::$pdo)) {
-            self::$pdo = new \PDO('mysql:host=db;dbname=teste_backend;charset=utf8mb4', 'teste_backend', 'teste_backend', [
+            $dsn = 'mysql:host=db;dbname=teste_backend;charset=utf8mb4';
+            self::$pdo = new \PDO($dsn, 'teste_backend', 'teste_backend', [
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ
             ]);
         }
